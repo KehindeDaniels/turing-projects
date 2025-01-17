7101d3b64db79b71edfc93c7ea6feb2746fe02f8
kehindeDaniels <kehindedaniels501@gmail.com>
2025-01-17 10:18:50 +0100

    turn 4

--- just-right/modelb2/src/i18n.js
+++ just-right/modelb2/src/i18n.js
@@ -1,14 +1,16 @@
 // src/i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import en from "./locales/en.json";
+import ar from "./locales/ar.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {
-      translation: require("./locales/en.json"),
+      translation: en,
     },
     ar: {
-      translation: require("./locales/ar.json"),
+      translation: ar,
     },
   },
   lng: "en",
