9e91ae690d274e698783b551964593e37dcc1a31
kehindedaniels <kehindedaniels501@gmail.com>
2025-01-23 05:06:57 +0100

    done with readme project

--- readme-hub/modelb/src/i18n.js
+++ readme-hub/modelb/src/i18n.js
@@ -1,19 +1,30 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend";
+import en from "../public/locales/en/translation.json";
+import es from "../public/locales/es/translation.json";
+import fr from "../public/locales/fr/translation.json";
+import de from "../public/locales/de/translation.json";
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    debug: true,
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false,
+i18n.use(initReactI18next).init({
+  debug: true,
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: {
+    en: {
+      translation: en,
     },
-    backend: {
-      loadPath: "/locales/{{lng}}/translation.json",
+    es: {
+      translation: es,
     },
-  });
+    fr: {
+      translation: fr,
+    },
+    de: {
+      translation: de,
+    },
+  },
+});
 
 export default i18n;
